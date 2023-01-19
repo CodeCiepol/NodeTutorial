60466004f976f591601350d228c0e728806b50b0
Ciepol <adamulicho@gmail.com>
2023-01-19 02:06:00 +0100

    FirstServer

---
+++ First server/app.js
@@ -0,0 +1,16 @@
+// we need to import modules (not using "import" )
+
+const http = require("http");
+function rqListener(req, res) {
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js</h1></body>")
+  res.write("</html>");
+  res.end()
+
+}
+const server = http.createServer(rqListener);
+
+server.listen(3000);
