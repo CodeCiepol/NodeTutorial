d8d22c4c94bf9d8aa6f4dfb71077b9e1169a4910
Ciepol <adamulicho@gmail.com>
2023-01-20 00:29:58 +0100

    First server

--- First server/app.js
+++ First server/app.js
@@ -5,7 +5,7 @@ function rqListener(req, res) {
   console.log(req.url, req.method, req.headers);
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
+  res.write("<head><title>My First Page</title><head>");
   res.write("<body><h1>Hello from my Node.js</h1></body>")
   res.write("</html>");
   res.end()
