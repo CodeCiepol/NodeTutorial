018531ca9811095a36d7be1d286f38ff62ccc2b3
CiepLap <adamulicho@gmail.com>
2023-01-30 16:54:06 +0100

    ExpressViewsBymyself

---
+++ ExpressViewsByMyself/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use("/users",(req,res,next)=>{
+res.sendFile(path.join(__dirname,'views',"users.html"))
+})
+
+app.use("/",(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'views',"users.html"))
+    })
+
+app.listen(3000)
