d89e4f0725a7a6aa1a97ddaa5783036f8804e9ee
CiepLap <adamulicho@gmail.com>
2023-02-08 17:59:37 +0100

    MVC controllers

--- MVC/app.js
+++ MVC/app.js
@@ -1,25 +1,24 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const errorController = require('./controllers/error');
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.set('view engine', 'ejs') //seting default templating engine
-app.set('views', 'views')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.render('404',{pageTitle: "not found",path:""}) //it will use default templating engine (which is set in app.js)
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(3000)
+app.use(errorController.get404);
+
+app.listen(3000);
