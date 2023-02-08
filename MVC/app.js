572edc59cab48d3ea120124b4622b8c8de11cdd2
CiepLap <adamulicho@gmail.com>
2023-02-08 16:22:28 +0100

    MVCInitial

---
+++ MVC/app.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.set('view engine', 'ejs') //seting default templating engine
+app.set('views', 'views')
+
+app.use((req, res, next) => {
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.render('404',{pageTitle: "not found",path:""}) //it will use default templating engine (which is set in app.js)
+})
+
+app.listen(3000)
